26769da6ae509085f650db666c22c0c6e920fa6f
alexandramorin <alexandra.morin.5@umontreal.ca>
2023-04-12 20:32:29 -0400

    cli complet.

--- cli.java
+++ cli.java
@@ -45,11 +45,63 @@ public class cli {
     fromServer.close();
   } catch( IOException e ) {
   }
-  System.out.println( "*** Bienvenue au portail d'inscription de cours de l'UDEM ***" );
-  System.out.println( "Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours" );
-  for( int i = 0; i < code.size(); i++ ) {
-    System.out.println( i + ". " + session.get(i) );
+
+  Scanner keyboard = new Scanner(System.in);
+  while( true ) {
+    System.out.println( "*** Bienvenue au portail d'inscription de cours de l'UDEM ***" );
+    System.out.println( "Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours" );
+
+    List<String>Saison = new ArrayList<String>();
+    for( int i = 0; i < code.size(); i++ ) {
+      Saison.add( session.get(i) );
+    }
+    List<String> SaisonSansDoublon = new ArrayList<>( new HashSet<>(Saison));
+    for( int i = 0; i < SaisonSansDoublon.size(); i++ ) {
+      System.out.println( i + 1 + ". " + SaisonSansDoublon.get(i) );
+    }
+    System.out.print("> Choix: ");
+    int Choix = keyboard.nextInt() - 1;
+
+    System.out.println("Les cours offserts pendant la sesion " + SaisonSansDoublon.get(Choix) + " sont:");
+
+    int Choix2 = 0;
+    for( int i = 0; i < session.size(); i++ )  {
+      if ( session.get(i).equals( SaisonSansDoublon.get(Choix)) ) {
+        System.out.println( Choix2+1 + ". " + code.get(i) + " " + nom.get(i) );
+        Choix2++;
+       }
+    }
+    System.out.print("> Choix: ");
+    int Choix3 = keyboard.nextInt() - 1;
+
+    System.out.println( "1. Consulter les cours oofferts pour una autre session" );
+    System.out.println( "2. Inscription a un cours" );
+    System.out.print( "> Choix: " );
+    int Choix4 = keyboard.nextInt() - 1;
+    String rien = keyboard.nextLine();
+    if ( Choix4 == 0 ) {
+      continue;
+    }
+    break;
   }
+  System.out.print( "Veuillez saisir votre prenom: " );  
+  String Prenom = keyboard.nextLine();
+
+  System.out.print( "Veuillez saisir votre nom: " );  
+  String Nom = keyboard.nextLine();
+
+  System.out.print( "Veuillez saisir votre email: " );  
+  String Courriel = keyboard.nextLine();
+
+  System.out.print( "Veuillez saisir votre matricule: " );  
+  int matricule = keyboard.nextInt();
+
+  System.out.print( "Veuillez saisir le code du cours: " ); 
+  String rien = keyboard.nextLine(); 
+  String codeDuCours = keyboard.nextLine();
+
+  System.out.println( "F'licitations! Inscription reussie de " + Prenom + " au cours " + codeDuCours );
+
 }
 }
 
